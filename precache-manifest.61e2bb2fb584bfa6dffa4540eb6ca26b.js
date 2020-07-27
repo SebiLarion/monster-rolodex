3284617bb096afe6e451fb90302b15781cf459fc
PETRU EUSEBIU LARION <eusebiu.larion@rcs-rds.local>
2020-07-27 15:49:16 +0300

    Updates

---
+++ precache-manifest.61e2bb2fb584bfa6dffa4540eb6ca26b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57ecc343312a16219595fef34da441e9",
+    "url": "/github.io/monster-rolodex/index.html"
+  },
+  {
+    "revision": "0a85d1c2a6c90e650568",
+    "url": "/github.io/monster-rolodex/static/css/main.3281d365.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/github.io/monster-rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/monster-rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a85d1c2a6c90e650568",
+    "url": "/github.io/monster-rolodex/static/js/main.929cc198.chunk.js"
+  },
+  {
+    "revision": "85e18b6363b223dd464f",
+    "url": "/github.io/monster-rolodex/static/js/runtime-main.a6c900e3.js"
+  }
+]);
